75d5f37eeec51cf397e2e9210c68c634f3c42cd5
Ihor Krasnov <ihor_krasnov@epam.com>
2022-11-15 14:28:39 +0200

    chore: added dependencies, configured paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '~': './',
+          assets: './src/assets',
+          components: './src/components',
+          constants: './src/constants',
+          navigation: './src/navigation',
+          screens: './src/screens',
+          types: './src/types',
+          utils: './src/utils',
+          hooks: './src/hooks',
+          services: './src/services',
+          providers: './src/providers',
+        },
+      },
+    ],
+  ],
 };
